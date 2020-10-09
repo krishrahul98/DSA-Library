ace638c481cfa51ce37a173df27b52cf48087a86
Rahul Chauhan <66589195+therc01@users.noreply.github.com>
2020-10-09 16:18:46 +0530

    shell sort in python

---
+++ Algorithms/Sorting/Shell_Sort/shell_sort.py
@@ -0,0 +1,36 @@
+def shellSort(arr): 
+  
+
+    n = len(arr) 
+    gap = n//2
+  
+
+    while gap > 0: 
+  
+        for i in range(gap,n): 
+  
+            
+            temp = arr[i] 
+  
+            
+            j = i 
+            while  j >= gap and arr[j-gap] >temp: 
+                arr[j] = arr[j-gap] 
+                j -= gap 
+  
+            arr[j] = temp 
+        gap //= 2
+  
+  
+arr = [12,45,32,21,56,87,43] 
+  
+n = len(arr) 
+print ("Array before sorting:") 
+for i in range(n): 
+    print(arr[i]), 
+  
+shellSort(arr) 
+  
+print ("\nArray after sorting:") 
+for i in range(n): 
+    print(arr[i]), 
