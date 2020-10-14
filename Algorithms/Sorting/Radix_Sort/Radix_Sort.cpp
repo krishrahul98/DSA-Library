19770050a8ca1788f3e052476eacbbde458845a3
Dwarikanathmishra <dwarikanathmishra7@gmail.com>
2020-10-14 23:51:13 +0530

    code refactor

--- Algorithms/Sorting/Radix_Sort/Radix_Sort.cpp
+++ Algorithms/Sorting/Radix_Sort/Radix_Sort.cpp
@@ -64,9 +64,18 @@ void print(int arr[], int n)
 
 // Driver Code
 int main()
-{
-    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
-    int n = sizeof(arr) / sizeof(arr[0]);
+{   
+    cout << "Enter the size of the array: " << endl ;
+    int n ; cin >> n;
+
+    int arr[n];
+
+    cout << "Enter space separated inputs for the array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i] ;
+    }
+    
 
     // Function Call
     radixsort(arr, n);
