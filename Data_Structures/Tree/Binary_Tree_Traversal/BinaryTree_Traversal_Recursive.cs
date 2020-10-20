1b752fe92755713a3e783a520471ecc2867ae1f5
Vijay <may31.py@gmail.com>
2020-10-20 13:10:02 +0530

    Update BinaryTree_Traversal_Recursive.cs

--- Data_Structures/Tree/Binary_Tree_Traversal/BinaryTree_Traversal_Recursive.cs
+++ Data_Structures/Tree/Binary_Tree_Traversal/BinaryTree_Traversal_Recursive.cs
@@ -1,79 +1,115 @@
-// C# program for different tree traversals 
-using System;
-
-/* Class containing left and right child of current node and key value*/
-
-class Node {
-  public int key;
-  public Node left,
-  right;
-  public Node(int item) {
-    key = item;
-    left = right = null;
-  }
-}
-
-class BinaryTree {
-  Node root;
-  BinaryTree() {
-    root = null;
-  }
-  /* Given a binary tree, print its nodes according to the "bottom-up" postorder traversal. */
-  void printPostorder(Node node) {
-    if (node == null) return;
-    // first recur on left subtree 
-    printPostorder(node.left);
-    // then recur on right subtree 
-    printPostorder(node.right);
-    // now deal with the node 
-    Console.Write(node.key + " ");
-  }
-  /* Given a binary tree, print 
-its nodes in inorder*/
-  void printInorder(Node node) {
-    if (node == null) return;
-    /* first recur on left child */
-    printInorder(node.left);
-    /* then print the data of node */
-    Console.Write(node.key + " ");
-    /* now recur on right child */
-    printInorder(node.right);
-  }
-  /* Given a binary tree, print 
-its nodes in preorder*/
-  void printPreorder(Node node) {
-    if (node == null) return;
-    /* first print data of node */
-    Console.Write(node.key + " ");
-    /* then recur on left sutree */
-    printPreorder(node.left);
-    /* now recur on right subtree */
-    printPreorder(node.right);
-  }
-  // Wrappers over above recursive functions 
-  void printPostorder() {
-    printPostorder(root);
-  }
-  void printInorder() {
-    printInorder(root);
-  }
-  void printPreorder() {
-    printPreorder(root);
-  }
-
-  // Driver Code 
-  static public void Main(String[] args) {
-    BinaryTree tree = new BinaryTree();
-    tree.root = new Node(1);
-    tree.root.left = new Node(2);
-    tree.root.right = new Node(3);
-    tree.root.left.left = new Node(4);
-    tree.root.left.right = new Node(5);
-    Console.WriteLine("Preorder traversal " + "of binary tree is ");
-    tree.printPreorder();
-    Console.WriteLine("\nInorder traversal " + "of binary tree is ");
-    tree.printInorder();
-    Console.WriteLine("\nPostorder traversal " + "of binary tree is ");
-    tree.printPostorder();
-  }
-}
+class Node
+    {
+        public int item;
+        public Node left;
+        public Node right;
+        public void display()
+        {
+            Console.Write("[");
+            Console.Write(item);
+            Console.Write("]");
+        }
+    }
+    class Tree
+    {
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+        public void Insert(int id)
+        {
+            Node newNode = new Node();
+            newNode.item = id;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (id < current.item)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        public void Preorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Console.Write(Root.item + " ");
+                Preorder(Root.left);
+                Preorder(Root.right);
+            }
+        }
+        public void Inorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Inorder(Root.left);
+                Console.Write(Root.item + " ");
+                Inorder(Root.right);
+            }
+        }
+        public void Postorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Postorder(Root.left);
+                Postorder(Root.right);
+                Console.Write(Root.item + " ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree BST = new Tree();
+            BST.Insert(30);
+            BST.Insert(35);
+            BST.Insert(57);
+            BST.Insert(15);
+            BST.Insert(63);
+            BST.Insert(49);
+            BST.Insert(89);
+            BST.Insert(77);
+            BST.Insert(67);
+            BST.Insert(98);
+            BST.Insert(91);
+            Console.WriteLine("Inorder Traversal : ");
+            BST.Inorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Preorder Traversal : ");
+            BST.Preorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Postorder Traversal : ");
+            BST.Postorder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.ReadLine();
+        }
+    }
