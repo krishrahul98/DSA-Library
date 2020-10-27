3a5fb591a0e5e904a0d6f70cb13be84ef8100c9e
felipe <giribroba@gmail.com>
2020-10-27 02:08:16 -0300

    Create Inverion_Count.cs

---
+++ Data_Structures/Array/Inversion_Count/Invertion_Count.cs
@@ -0,0 +1,21 @@
+﻿
+class Inversion_Count
+{
+    public static int invertionCount(int[] input)
+    {
+        var count = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (input[i] > input[j])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
