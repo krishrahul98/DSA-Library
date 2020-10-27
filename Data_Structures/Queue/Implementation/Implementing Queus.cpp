b97fec18829ae9cd36c76d92f2af3a34e68a6b8a
Rudra Patil <rudrapatil1@gmail.com>
2020-10-27 15:22:55 +0530

    Alternate Implementing queues

---
+++ Data_Structures/Queue/Implementation/Implementing Queus.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#define SIZE 5
+
+using namespace std;
+
+class Queue
+{
+	private:
+		int items[SIZE], front, rear;
+	public:
+		Queue()
+		{
+			front = -1;
+			rear = -1;
+		}
+bool isFull()
+{
+	if(front == 0 && rear== SIZE-1)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool isEmpty()
+{
+	if (front==1)
+	{
+		return true;
+	}
+	else
+	return false;
+}
+void enQueue(int element)
+{
+	if (isFull())
+	{
+		cout<<"Queue is Full";
+	}
+	else
+	{
+		if (front == -1)
+		{
+			front = 0;
+			rear++;
+			items[rear] = element;
+			cout<<endl<<"Inserted"<<element<<endl;
+		}
+	}
+}
+int deQueue()
+{
+	int element;
+	if(isEmpty())
+	{
+		cout<<"Queue is empty"<<endl;
+		return(-1);
+	}
+	else
+	{
+		element = items[front];
+		if(front>=rear)
+		{
+			front = -1;
+			rear = -1;
+		}
+		else
+		{
+			front++;
+		}
+		cout<<endl<<"Deleted -> "<<element<<endl;
+		return(element);
+	}
+}
+void Display()
+{
+	int i;
+	if (isEmpty())
+	{
+		cout<<endl<<"Empty Queue"<<endl;
+	}
+	else
+	{
+		cout<<endl<<"Front index ->"<<front;
+		cout<<endl<<"Items->";
+		for(i=front;i<=rear;i++)
+		{
+			cout<<items[i]<<" ";
+		}
+		cout<<endl<<"Rear Index->"<<rear<<endl;
+	}
+}
+};
+
+int main()
+{
+	Queue q;
+
+	q.enQueue(1);
+	q.enQueue(2);
+	q.enQueue(3);
+	q.enQueue(4);
+	q.enQueue(5);
+	q.enQueue(6);
+	
+	q.Display();
+	
+	q.deQueue();
+	
+	q.Display();
+	
+	return 0;
+}
+
+
+
