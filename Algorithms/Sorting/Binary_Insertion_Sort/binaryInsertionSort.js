3ad9e2882822c7ad600b108ca819024227db4533
OussamaSALAHOUELHADJ <samaoussama24@gmail.com>
2021-10-02 21:58:56 +0100

    replacing insertion sort Algorithm with Binary Insertion Sort Algorithm

--- Algorithms/Sorting/Binary_Insertion_Sort/binaryInsertionSort.js
+++ Algorithms/Sorting/Binary_Insertion_Sort/binaryInsertionSort.js
@@ -1,25 +1,39 @@
-//Swap function swaps two positions in an arra and returns the new array.
-function swap(array, firstIndex, secondIndex) {
-    let exchParam = array[firstIndex];
-    array[firstIndex] = array[secondIndex];
-    array[secondIndex] = exchParam;
+//binaryFindIndex is a function that performe a binary search and return an index where a given value(index of a value) should be inserted. 
+function binaryFindIndex(array, index) {
+    let lowerIndex = 0;
+    let middleIndex = 0;
+    let upperIndex = index - 1;
+    let nextValue = array[index]
+    let nextValueIndex;
+    let indexFound = false
+    index >= array.length ? (nextValueIndex = -1, indexFound = true) : null;
+    while (upperIndex >= lowerIndex && !indexFound) {
+        middleIndex = Math.floor((upperIndex + lowerIndex) / 2);
+        if (upperIndex === lowerIndex || nextValue === array[middleIndex]) {
+            nextValue > array[middleIndex] ? (nextValueIndex = middleIndex + 1, indexFound = true) : (nextValueIndex = middleIndex, indexFound = true)
+        } else if (nextValue < array[middleIndex]) {
+            upperIndex = middleIndex - 1;
+        } else if (nextValue > array[middleIndex]) {
+            lowerIndex = middleIndex + 1;
+        }
+    }
 
+    return indexFound ? nextValueIndex : lowerIndex;
+}
+
+//insertAtIndex function takes an array, an index where the element should be inserted and elementIndex to get the element and also to delete the element at the old index.
+function insertAtIndex(array, index, elementIndex) {
+    array.splice(index, 0, array[elementIndex]);
+    array.splice(elementIndex + 1, 1)
     return array;
 }
 
-function binarySort(array) {
-    let firstIndex = 1;
-    while (firstIndex < array.length) {
-        let secondIndex = firstIndex;
-        while (secondIndex > 0 && array[secondIndex - 1] > array[secondIndex]) {
-            array = swap(array, secondIndex - 1, secondIndex)
-            secondIndex--
-        }
-        firstIndex++
+//binaryInsertionSort function takes an array and sort it using binary search to find the next index. 
+function binaryInsertionSort(array) {
+    let arr = array.slice(0);
+    for (let index = 1; index < arr.length; index++) {
+        const insertionIndex = binaryFindIndex(arr, index, arr[index]);
+        arr = insertAtIndex(arr, insertionIndex, index);
     }
-    return array
+    return arr;
 }
-
-const array = [6, 5, 3, 0, 0, 1, 8, 7, 5, 2, 4];
-const sortedArray = binarySort(array)
-console.log(sortedArray);
