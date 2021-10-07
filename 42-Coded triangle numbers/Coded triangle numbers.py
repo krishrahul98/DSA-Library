932d802d384fc2f0a9baca6472eacb4a180a8212
gattsu997 <76897671+gattsu997@users.noreply.github.com>
2021-10-07 14:15:45 +0530

    Update Coded triangle numbers.py

--- 42-Coded triangle numbers/Coded triangle numbers.py
+++ 42-Coded triangle numbers/Coded triangle numbers.py
@@ -1,12 +1,12 @@
 word=input().lower()
-sum=0
+summ=0
 d=0
 L=list(word)
 for ele in L:
     num=int(ord(ele)-96)
-    sum+=num
-for i in range (0,sum):
-    if i*(i+1)/2==sum:
+    summ+=num
+for i in range (0,summ):
+    if i*(i+1)/2==summ:
         print("triangle word")
         d+=1
 if d==0:
